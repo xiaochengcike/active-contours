eda751103d18172ca4902ade8c5e1ec5872aa513
whyguu <whyguu@sina.com>
2018-01-10 16:06:06 +0800

    Update MyImagePreprocess.m

--- MyImagePreprocess.m
+++ MyImagePreprocess.m
@@ -23,6 +23,11 @@ f22 = imresize(Img,[500,500]);
 imwrite(f22,'mypictures/haha1.jpg');
 
 
+% fn='mypictures/qh_wy_s3_c1_gr.png';
+% img = imread(fn);
+% h = fspecial('gaussian', [5, 5], 1.0);
+% img2 = imfilter(img, h, 'replicate');
+% imwrite(img2, 'mypictures/s3_fl.png')
 %% %%%%%
 % figure
 % ed = size(phi);
